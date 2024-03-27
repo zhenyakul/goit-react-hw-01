c106f4e5b195557e4a07733dc952684692b701fe
Yevhenii <zhekaboy04@gmail.com>
2024-03-27 12:21:43 +0100

    bugfix2.0

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -2,22 +2,22 @@ import css from "./Transactions.module.css";
 
 const Transactions = ({ transactions }) => {
   return (
-    <table>
+    <table className={css.table}>
       <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+        <tr className={css.tr}>
+          <th className={css.th}>Type</th>
+          <th className={css.th}>Amount</th>
+          <th className={css.th}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {transactions.map((transaction) => {
           return (
-            <tr>
-              <td>{transaction.type}</td>
-              <td>{transaction.amount}</td>
-              <td>{transaction.currency}</td>
+            <tr className={css.tr}>
+              <td className={css.td}>{transaction.type}</td>
+              <td className={css.td}>{transaction.amount}</td>
+              <td className={css.td}>{transaction.currency}</td>
             </tr>
           );
         })}
